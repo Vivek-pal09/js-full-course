a5d75883f26ee4b845636d6b43491c8dfc319f52
Vivek-pal09 <110651790+Vivek-pal09@users.noreply.github.com>
2025-03-16 12:13:50 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,31 @@
+"use strict";    //treat all js code as newer version 
+
+// alert(3+3)   // we are using modejs ,not browser
+
+console.log(3
+    
+    +3)  //code readibility should be high
+
+console.log("Vivek")
+
+
+let name = "vivek"
+let age = 12
+let isLoggedin = false
+
+console.log(age);
+
+
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+//symbol =>unique
+
+// object 
+
+console.log(typeof undefined ); // undefined
+console.log(typeof null);  //object
+
