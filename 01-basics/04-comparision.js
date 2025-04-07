66e8eacf4f5d31c26ccc2dc6da0f403aff85ad16
Vivek-pal09 <110651790+Vivek-pal09@users.noreply.github.com>
2025-04-07 13:47:09 +0000

    datatype summary

---
+++ 01-basics/04-comparision.js
@@ -0,0 +1,11 @@
+console.log(2>1);
+console.log("2">1);
+
+
+console.log(null > 0);
+console.log( null == 0);  // this == equal sign have dfferent meaning and use  
+console.log( null >= 0);  // this comparision sign have different meaning and use 
+
+console.log(undefined==1)
+
+console.log("2" === 2);  // this === thiple equal sign have defferent meaning and use this sign check the datatypes 
---
+++ 01-basics/datatype-summary.js
@@ -0,0 +1,43 @@
+// primitive
+
+// 7 types of categoties   1)String  2)Number 3)Boolean 4)null 5)undefined 6)Symbol  7)BigInt
+
+const score=100
+
+let scorevalue = 100.4
+
+const isLoggedIn =false
+const outsideTemp =null
+
+let userMail;
+
+const id =Symbol('1233')
+const idVivek =Symbol('1233')
+
+console.log(id === idVivek)
+
+
+// const bigNumber = 1815
+
+
+//Reference (Non Primitive)
+// 1)Array 2)Objects
+
+const gods =['RaONE', 'shaktiman', 'naagraj', 'dog']
+
+// what is object the whole part in {} this bracket are object
+
+let myObj ={
+    name:'xyz',
+    age:23,
+}
+const myfunction= function(){
+    console.log("Hello Word")
+
+ }
+ console.log(typeof bigNumber)
+
+
+ //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+ 
